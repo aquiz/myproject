ef8e798f20e349dd0d9da026a87dbb466818366e
ezemollel <ezemollel@yahoo.com>
2014-08-01 18:01:42 +0300

    added addbus.php

--- conn.php
+++ conn.php
@@ -3,10 +3,9 @@ $host="localhost";
 $db="transport_system";
 
 $user="root";
-$pass="";
+$pass="theloneranger";
 
 $link=mysql_connect($host,$user,$pass) or die("could not connect");
 mysql_select_db($db,$link) or die("could not connect, do you have a database?");
 
-echo "connected";
 ?>
